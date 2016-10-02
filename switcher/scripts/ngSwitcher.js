39d25aad4f4f2280f363721e91b2cceeb0c5ae0d
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-02 12:37:27 +0800

    merge turnOn and turnOff funcrions into one function

--- switcher/scripts/ngSwitcher.js
+++ switcher/scripts/ngSwitcher.js
@@ -3,34 +3,53 @@
 // 2. 点击中间button状态切换的时候，button两边的文字样式也要随之改变
 // 3. 可以disable切换的功能，使之不可点击
 
-app.run(function($templateCache) {
-  $templateCache.put('switcher.html', 
+app.run(function ($templateCache) {
+  $templateCache.put('switcher.html',
     '<div class="switcher" ng-class="{active:switcherStatus, disabled: disableStatus}">' +
-      '<span class="switcher-label front" ng-click="turnOn()">Turn On</span>' +
-        '<label class="switcher-line">' +
-          '<input ' +
-            'class="inner-input" ' +
-            'type="checkbox" ' +
-            'ng-model="switcherStatus"' +
-            'ng-disabled="disableStatus"' +
-            'ng-change="changeFunc()">' +
-          '</input>' +
-        '</label>' +
-      '<span class="switcher-label back" ng-click="turnOff()">Turn Off</span>' +
+    '<span class="switcher-label front" ng-click="switchTurn(false)">Turn On</span>' +
+    '<label class="switcher-line">' +
+    '<input ' +
+    'class="inner-input" ' +
+    'type="checkbox" ' +
+    'ng-model="switcherStatus"' +
+    'ng-disabled="disableStatus"' +
+    'ng-change="changeFunc()">' +
+    '</input>' +
+    '</label>' +
+    '<span class="switcher-label back" ng-click="switchTurn(true)">Turn Off</span>' +
     '</div>'
   );
 });
 
 
-app.directive('switcher', [function() {
+app.directive('switcher', [function () {
   return {
     restrict: 'E',
     templateUrl: 'switcher.html',
     scope: {
-
+      turnOn: '&'
     },
-    link: {
+    link: function (scope, element, attr) {
+      
+      scope.switchTurn = function (value) {
+        if (!scope.disableStatus) {
+          if (value != scope.switcherStatus) {
+            scope.switcherStatus = value;
+          }
+        }
+      }
+
+
 
     }
   }
 }]);
+
+
+
+
+
+
+
+
+
