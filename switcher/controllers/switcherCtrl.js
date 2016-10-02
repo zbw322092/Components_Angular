923a64c1f33d7baf1ce159604d655975a21b528b
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-02 11:50:12 +0800

    define ng-click on switcher label

--- switcher/controllers/switcherCtrl.js
+++ switcher/controllers/switcherCtrl.js
@@ -3,5 +3,25 @@ var app = angular.module('switcher_app', []);
 app.controller('switcherCtrl', ['$scope', function($scope) {
 	console.log($scope.switcherStatus);
 
+	$scope.changeFunc = function() {
+		console.log($scope.switcherStatus);
+	}
+
+	$scope.turnOn = function() {
+		if (!$scope.disableStatus) {
+			if($scope.switcherStatus) {
+				$scope.switcherStatus = false;
+			}
+		}
+	}
+
+	$scope.turnOff = function() {
+		if (!$scope.disableStatus) {
+			if(!$scope.switcherStatus) {
+				$scope.switcherStatus = true;
+			}
+		}
+	}
+
 
 }]);
