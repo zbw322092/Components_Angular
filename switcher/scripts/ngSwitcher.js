bfe0f3dcf18574e6e917e6ed4f1d114cacd7cb38
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-02 12:07:54 +0800

    start building directive

--- switcher/scripts/ngSwitcher.js
+++ switcher/scripts/ngSwitcher.js
@@ -3,3 +3,34 @@
 // 2. 点击中间button状态切换的时候，button两边的文字样式也要随之改变
 // 3. 可以disable切换的功能，使之不可点击
 
+app.run(function($templateCache) {
+  $templateCache.put('switcher.html', 
+    '<div class="switcher" ng-class="{active:switcherStatus, disabled: disableStatus}">' +
+      '<span class="switcher-label front" ng-click="turnOn()">Turn On</span>' +
+        '<label class="switcher-line">' +
+          '<input ' +
+            'class="inner-input" ' +
+            'type="checkbox" ' +
+            'ng-model="switcherStatus"' +
+            'ng-disabled="disableStatus"' +
+            'ng-change="changeFunc()">' +
+          '</input>' +
+        '</label>' +
+      '<span class="switcher-label back" ng-click="turnOff()">Turn Off</span>' +
+    '</div>'
+  );
+});
+
+
+app.directive('switcher', [function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'switcher.html',
+    scope: {
+
+    },
+    link: {
+
+    }
+  }
+}]);
