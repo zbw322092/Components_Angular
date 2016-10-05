d024d6a488086227a04ac709c390481399cd2a0c
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-05 11:33:52 +0800

    make label configurable

--- switcher/controllers/switcherCtrl.js
+++ switcher/controllers/switcherCtrl.js
@@ -1,26 +1,10 @@
 var app = angular.module('switcher_app', []);
 
-app.controller('switcherCtrl', ['$scope', function($scope) {
+app.controller('switcherCtrl', ['$scope', '$sce', function($scope, $sce) {
 	console.log($scope.switcherStatus);
 
 	$scope.changeFunc = function() {
 		console.log($scope.switcherStatus);
-	}
-
-	$scope.turnOn = function() {
-		if (!$scope.disableStatus) {
-			if($scope.switcherStatus) {
-				$scope.switcherStatus = false;
-			}
-		}
-	}
-
-	$scope.turnOff = function() {
-		if (!$scope.disableStatus) {
-			if(!$scope.switcherStatus) {
-				$scope.switcherStatus = true;
-			}
-		}
 	}
 
 
--- switcher/scripts/ngSwitcher.js
+++ switcher/scripts/ngSwitcher.js
@@ -6,32 +6,34 @@
 app.run(function ($templateCache) {
   $templateCache.put('switcher.html',
     '<div class="switcher" ng-class="{active:switcherStatus, disabled: disableStatus}">' +
-    '<span class="switcher-label front" ng-click="switchTurn(false)">Turn On</span>' +
-    '<label class="switcher-line">' +
-    '<input ' +
-    'class="inner-input" ' +
-    'type="checkbox" ' +
-    'ng-model="switcherStatus"' +
-    'ng-disabled="disableStatus"' +
-    'ng-change="changeFunc()">' +
-    '</input>' +
-    '</label>' +
-    '<span class="switcher-label back" ng-click="switchTurn(true)">Turn Off</span>' +
+    ' <span class="switcher-label front" ng-click="switcherTurn(false)" ng-bind-html="trustAsHtml(switcherFrontSign)"></span>' +
+    '   <label class="switcher-line">' +
+    '     <input ' +
+    '       class="inner-input" ' +
+    '       type="checkbox" ' +
+    '       ng-model="switcherStatus"' +
+    '       ng-disabled="disableStatus"' +
+    '       ng-change="changeFunc()">' +
+    '     </input>' +
+    '   </label>' +
+    ' <span class="switcher-label back" ng-click="switcherTurn(true)" ng-bind-html="trustAsHtml(switcherBackSign)"></span>' +
     '</div>'
   );
 });
 
 
-app.directive('switcher', [function () {
+app.directive('switcher', [ '$sce', function ($sce) {
   return {
     restrict: 'E',
     templateUrl: 'switcher.html',
     scope: {
-      turnOn: '&'
+      // turnOn: '&'
+      switcherFrontSign: '@',
+      switcherBackSign: '@'
     },
     link: function (scope, element, attr) {
-      
-      scope.switchTurn = function (value) {
+
+      scope.switcherTurn = function (value) {
         if (!scope.disableStatus) {
           if (value != scope.switcherStatus) {
             scope.switcherStatus = value;
@@ -39,17 +41,12 @@ app.directive('switcher', [function () {
         }
       }
 
+      scope.trustAsHtml = function(value) {
+        return $sce.trustAsHtml(value);
+      }
+
 
 
     }
   }
 }]);
-
-
-
-
-
-
-
-
-
