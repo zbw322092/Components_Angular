ac333521d2d859bac04f853195f0e7e2975c6998
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-06 18:43:59 +0800

    init directives

---
+++ tabs/scripts/ngTabs.js
@@ -0,0 +1,70 @@
+var i = 0, headerIndex = 0;
+var p = 0, bodyIndex = 0;
+
+app.directive('ngTabsHeader', function() {
+  return {
+    scope: false,
+    restrict: 'EAC',
+    link: function(scope, element, attrs) {
+      console.log(scope);
+      console.log(element);
+      console.log(attrs);
+
+      function tabHeaderIndex() {
+        return headerIndex = ++i;
+      }
+      tabHeaderIndex();
+      console.log('header index: ' + headerIndex);
+
+      var headerObject = {};
+
+      element.on('click', function(headerIndex) {
+        if (bodyIndex === headerIndex) {
+
+        }
+      });
+
+    }
+  }
+});
+
+
+app.directive('ngTabsBody', function() {
+  return {
+    scope: false,
+    restrict: 'EAC',
+    link: function(scope, element, attrs) {
+      console.log(scope);
+      console.log(element);
+      console.log(attrs);
+
+      function tabBodyIndex() {
+        return bodyIndex = ++p;
+      }
+      tabBodyIndex();
+      console.log('body index: ' + bodyIndex);
+
+      var bodyObject = {};
+
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
