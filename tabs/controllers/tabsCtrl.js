05d19c9a2942d71af84e20d56e00f38a6b4633b3
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-07 16:01:20 +0800

    modification

--- tabs/controllers/tabsCtrl.js
+++ tabs/controllers/tabsCtrl.js
@@ -2,47 +2,6 @@ var app = angular.module('tabs_app', []);
 
 app.controller('tabsCtrl', ['$scope', function ($scope) {
 
-  // $scope.defaultContentDisplay = {
-  //   tabOneContent: true,
-  //   tabTwoContent: false,
-  //   tabThreeContent: false,
-  //   tabFourContent: false
-  // }
-
-  // function setToFalse() {
-  //   angular.forEach($scope.defaultContentDisplay, function (value, key) {
-  //     if (value) {
-  //     	$scope.defaultContentDisplay[key] = false;
-  //     }
-  //   });
-  //   console.log($scope.defaultContentDisplay);
-  // }
-
-  // $scope.showTabContent = function (tabNum) {
-  //   switch (tabNum) {
-  //     case 'one':
-  //     	setToFalse();
-  //     	$scope.defaultContentDisplay.tabOneContent = true;
-  //       break;
-  //     case 'two':
-  //     	setToFalse();
-  //     	$scope.defaultContentDisplay.tabTwoContent = true;
-  //       break;
-  //     case 'three':
-  //     	setToFalse();
-  //     	$scope.defaultContentDisplay.tabThreeContent = true;
-  //       break;
-  //     case 'four':
-  //     	setToFalse();
-  //     	$scope.defaultContentDisplay.tabFourContent = true;
-  //       break;
-  //     default:
-  //     	window.alert('No Matches!');
-  //     	break;
-  //   }
-
-  // }
-
 }]);
 
 
