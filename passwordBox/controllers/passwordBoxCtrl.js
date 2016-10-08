df989d6d299b10fbfa4ffdae82dcd6ce10b47e94
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-08 21:48:21 +0800

    input focus

--- passwordBox/controllers/passwordBoxCtrl.js
+++ passwordBox/controllers/passwordBoxCtrl.js
@@ -1,7 +1,18 @@
 var app = angular.module('password_box_app', []);
 
-app.controller('passwordBoxCtrl', ['$scope', function ($scope) {
-	
+app.controller('passwordBoxCtrl', ['$scope','$timeout', function ($scope, $timeout) {
+	var inputDom = document.getElementsByClassName('password-input');
+	// prototype chain
+	console.log($scope);
+	console.log($scope.$apply);
+
+	console.log(inputDom);
+	console.log(typeof inputDom);
+
+
+	$scope.focusToInput = function() {
+		inputDom[0].focus();
+	}
 
 
 }]);
