3e410c472a699721e6adfb2bf8e42a72aef577e5
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-09 14:47:15 +0800

    build directive

--- passwordBox/controllers/passwordBoxCtrl.js
+++ passwordBox/controllers/passwordBoxCtrl.js
@@ -1,33 +1,12 @@
 var app = angular.module('password_box_app', []);
 
 app.controller('passwordBoxCtrl', ['$scope','$timeout', function ($scope, $timeout) {
-	var inputDom = document.getElementsByClassName('password-input');
-	// prototype chain
-	console.log($scope);
-	console.log($scope.$apply);
-
-	console.log(inputDom);
-	console.log(typeof inputDom);
-
-
-	$scope.focusToInput = function() {
-		inputDom[0].focus();
-	}
-
-	$scope.passwordDot = new Array('','','','','','');
-	
-	$scope.inputValueFunc = function() {
-		var passwordArray =  $scope.inputedValue.split("");
-		for (var i = 0; i < passwordArray.length; i++) {
-			$scope.passwordDot[i] = '*';
-		}
-	}
 
 }]);
 
 
 
-
+ 
 
 
 
--- passwordBox/scripts/passwordBox.js
+++ passwordBox/scripts/passwordBox.js
@@ -4,4 +4,47 @@
 // 3. 输入的密码值是以黑色实心圆来显示的
 // 4. 输入完六位之后自动把密码框里面的密码发往后台进行验证
 // 5. 输入完密码之后密码输入框自动消失（可配置）
-// 6. 
+
+app.directive('passwordBox', ['$timeout', function($timeout) {
+	return {
+		restrict: 'AE',
+		scope: {
+
+		},
+		templateUrl: 'passwordBox.html',
+		link: function(scope, element, attrs) {
+			
+			var inputDom = document.getElementsByClassName('password-input');
+			scope.focusToInput = function() {
+				inputDom[0].focus();
+			}
+
+			scope.passwordDot = new Array('','','','','','');
+
+			scope.inputValueFunc = function() {
+				if (scope.inputedValue.length < 6) {
+					var passwordArray =  scope.inputedValue.split("");
+					for (var i = 0; i < passwordArray.length; i++) {
+						scope.passwordDot[i] = '*';
+					}
+				} else if (scope.inputedValue.length = 6) {
+					scope.passwordDot[5] = '*'
+					$timeout(function() {
+						window.alert('Input Done');
+					});
+				}
+			}			
+
+		}
+	}
+}]);
+
+app.run(['$templateCache', function($templateCache) {
+	$templateCache.put('passwordBox.html', 
+	  '<ul class="password-list" ng-click="focusToInput()">' + 
+	    '<li ng-repeat="passwordUnit in passwordDot track by $index" ng-bind="passwordDot[$index]"></li>' +
+	  '</ul>' +
+	  '<input class="password-input" type="password" ng-keyup="inputValueFunc()" ng-model="inputedValue" maxlength="6">' +
+	  '</input>'
+	);
+}]);
