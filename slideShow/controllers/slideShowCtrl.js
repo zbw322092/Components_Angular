ef1dc04e6ee803913a62a32279695f3615c40355
Bowen Zhong <bz4e14@soton.ac.uk>
2016-10-21 12:05:17 +0800

    bring in jquery

--- slideShow/controllers/slideShowCtrl.js
+++ slideShow/controllers/slideShowCtrl.js
@@ -4,18 +4,20 @@ app.controller('slideShowCtrl', ['$scope', function ($scope) {
 
 	var elementWarpper = angular.element;
 
-	console.log(document.getElementsByClassName('slide-unit'));
-	console.log(document.getElementsByClassName('slide-unit').length);
+	// jquery
+	var $slideShowDom = $(".display-area");
+	var slideUnits = $slideShowDom.find('.slide-unit');
+	console.log(slideUnits);
 
-	var slideUnits = document.getElementsByClassName('slide-unit');
-	var slideUnitsLength = document.getElementsByClassName('slide-unit').length;
 
-	var unitHeight = elementWarpper(slideUnits[0]).css('height');
-	console.log(unitHeight);
-	console.log(slideUnits[0]);
+	function slideAnimation() {
+		$slideShowDom.find('.slide-unit:first').animate({marginTop: '-20px'}, 1000, function() {
+			window.alert('animation done');
+		});
+	}
 
-	var unitHeight2 = slideUnits[0].style;
-	console.log(unitHeight2);
+	slideAnimation();
+
 
 }]);
 
